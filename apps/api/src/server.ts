042fe85c146b9663ac1a83ad73a99bb94ea06031
Dian Rahmaji <dianrahmaji@gmail.com>
2024-12-22 16:41:01 +0700

    feat(chapter-2): implement Tag entity

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,7 @@
-import { MikroORM, wrap } from "@mikro-orm/sqlite";
+import { MikroORM } from "@mikro-orm/sqlite";
 import { User } from "./modules/user/user.entity.js";
 import { Article } from "./modules/article/article.entity.js";
+import { Tag } from "./modules/article/tag.entity.js";
 
 const orm = await MikroORM.init();
 
@@ -19,6 +20,25 @@ em.create(Article, {
   author: user.id,
 });
 
+await em.flush();
+
+const myUser = await em.findOneOrFail(User, user.id, {
+  populate: ["articles"],
+});
+
+const [article] = myUser.articles;
+
+const newTag = em.create(Tag, {
+  name: "new",
+});
+const oldTag = em.create(Tag, {
+  name: "old",
+});
+
+article.tags.add(newTag, oldTag);
+await em.flush();
+
+article.tags.remove(oldTag);
 await em.flush();
 
 orm.close();
