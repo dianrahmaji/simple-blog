2e6d90f60a8cb764c8412bca49d405fe9c9a79aa
Dian Rahmaji <dianrahmaji@gmail.com>
2024-12-22 16:40:12 +0700

    feat(chapter-2): add Tag entity

--- apps/api/src/mikro-orm.config.ts
+++ apps/api/src/mikro-orm.config.ts
@@ -3,6 +3,7 @@ import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 
 import { User } from "./modules/user/user.entity.js";
 import { Article } from "./modules/article/article.entity.js";
+import { Tag } from "./modules/article/tag.entity.js";
 
 const config: Options = {
   driver: SqliteDriver,
@@ -11,6 +12,7 @@ const config: Options = {
     //
     User,
     Article,
+    Tag,
   ],
   metadataProvider: TsMorphMetadataProvider,
   debug: true,
