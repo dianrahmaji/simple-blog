c517682b8e666a3c9355715e734be451028b44fc
Dian Rahmaji <dianrahmaji@gmail.com>
2024-12-23 22:00:13 +0700

    feat(chapter-3): add initial migration

--- apps/api/src/mikro-orm.config.ts
+++ apps/api/src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import { Options, SqliteDriver } from "@mikro-orm/sqlite";
 import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
 import { SeedManager } from "@mikro-orm/seeder";
+import { Migrator } from "@mikro-orm/migrations";
 
 import { User } from "./modules/user/user.entity.js";
 import { Article } from "./modules/article/article.entity.js";
@@ -17,7 +18,7 @@ const config: Options = {
   ],
   metadataProvider: TsMorphMetadataProvider,
   debug: true,
-  extensions: [SeedManager],
+  extensions: [SeedManager, Migrator],
 };
 
 export default config;
