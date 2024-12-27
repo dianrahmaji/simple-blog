1830b172917e04e76616105441829564f4c41525
Dian Rahmaji <dianrahmaji@gmail.com>
2024-12-27 22:40:33 +0700

    feat(article): add CRUD routes

--- apps/api/src/modules/article/routes.ts
+++ apps/api/src/modules/article/routes.ts
@@ -1,5 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { initORM } from "../../db.js";
+import { getUserFromToken, verifyArticlePermission } from "../common/utils.js";
+import { wrap } from "@mikro-orm/core";
+import { Article } from "./article.entity.js";
 
 export async function registerArticleRoutes(app: FastifyInstance) {
   const db = await initORM();
@@ -13,5 +16,55 @@ export async function registerArticleRoutes(app: FastifyInstance) {
     const [items, total] = await db.article.findAndCount({}, { limit, offset });
 
     return { items, total };
+  });
+
+  app.get("/:slug", async (request) => {
+    const { slug } = request.params as { slug: string };
+
+    return db.article.findOneOrFail(
+      { slug },
+      { populate: ["author", "comments.author", "text"] }
+    );
+  });
+
+  app.post("/:slug/comment", async (request) => {
+    const { slug, text } = request.params as { slug: string; text: string };
+    const author = getUserFromToken(request);
+
+    const article = await db.article.findOneOrFail({ slug });
+    const comment = await db.comment.create({ author, article, text });
+
+    article.comments.add(comment);
+
+    await db.em.flush();
+
+    return comment;
+  });
+
+  app.patch("/:id", async (request) => {
+    const user = getUserFromToken(request);
+    const { id } = request.params as { id: string };
+    const article = await db.article.findOneOrFail(+id);
+
+    verifyArticlePermission(user, article);
+
+    wrap(article).assign(request.body as Article);
+    await db.em.flush();
+
+    return article;
+  });
+
+  app.delete("/:id", async (request) => {
+    const user = getUserFromToken(request);
+    const { id } = request.params as { id: string };
+    const article = await db.article.findOneOrFail(+id);
+
+    if (!article) {
+      return { notFound: true };
+    }
+
+    verifyArticlePermission(user, article);
+
+    return { success: true };
   });
 }
--- apps/api/src/modules/common/utils.ts
+++ apps/api/src/modules/common/utils.ts
@@ -1,4 +1,5 @@
 import { FastifyRequest } from "fastify";
+import { Article } from "../article/article.entity.js";
 import { User } from "../user/user.entity.js";
 
 export function getUserFromToken(req: FastifyRequest): User {
@@ -7,6 +8,12 @@ export function getUserFromToken(req: FastifyRequest): User {
   }
 
   return req.user as User;
+}
+
+export function verifyArticlePermission(user: User, article: Article): void {
+  if (article.author.id !== user.id) {
+    throw new Error("You are not the author if this article!");
+  }
 }
 
 export class AuthError extends Error {}
